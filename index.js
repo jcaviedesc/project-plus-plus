f28c84a3057e5daaf6e2a90a32c9c619b1e6f68a
New Author jcaviedesc <jcamilocc97@gmail.com>
2021-12-06 14:28:38 -0500

    add semantics

--- index.js
+++ index.js
@@ -6,8 +6,30 @@ const myGrammar = ohm.grammar(contents);
 const sourceCode = fs.readFileSync('my-test-project.ppp', 'utf-8');
 const m = myGrammar.match(sourceCode);
 if (m.succeeded()) {
-  console.log('¡Nice!😁. Compile Project plus plus compiled successfully.');
+    console.log('¡Nice!😁. Project plus plus was compile successfully.');
+
+    const semantics = myGrammar.createSemantics();
+
+    semantics.addOperation('run', {
+        ProjectStatement(_, _1, keyAndValues, _2) {
+            return Object.assign({}, ...keyAndValues.children.map(c => c.run()));
+        },
+        KeyAndValue(key, _1, value, _2) {
+            return { [key.run()]: value.run() }
+        },
+        Key(a) {
+            return this.sourceString
+        },
+        Value(_1) {
+            return this.sourceString
+        },
+    })
+
+    const adapter = semantics(m);
+    const ast = adapter.run();
+
+    console.log("Result \n", ast);
 } else {
-  console.log("wrong syntax ☹️! \n");
-  console.log(m.message)
+    console.log("wrong syntax ☹️! \n");
+    console.log(m.message)
 }
