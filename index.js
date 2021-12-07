2d7acf7bac0deeadfa0ab0d87993432c4c132c7c
jcaviedes <jcamilocc97@gmail.com>
2021-12-07 00:40:22 -0500

    setup full grammar

--- index.js
+++ index.js
@@ -3,15 +3,34 @@ const ohm = require('ohm-js');
 const contents = fs.readFileSync('projectppGrammar.ohm', 'utf-8');
 const myGrammar = ohm.grammar(contents);
 
-const sourceCode = fs.readFileSync('my-test-project.ppp', 'utf-8');
+const sourceCode = fs.readFileSync('finalProject.ppp', 'utf-8');
 const m = myGrammar.match(sourceCode);
 if (m.succeeded()) {
-    console.log('¡Nice!😁. Project plus plus was compile successfully.');
-
     const semantics = myGrammar.createSemantics();
 
     semantics.addOperation('run', {
+        _iter(...item) {
+            return item
+        },
+        DocProject(proyect, activities, people, resources) {
+            console.log("activities",activities)
+            return {
+                proyecto: proyect.run(),
+                actividades: activities.children.map(e => e.run()),
+                personas: people.children.map(e => e.run()),
+                recursos: resources.children.map(e => e.run())
+            }
+        },
         ProjectStatement(_, _1, keyAndValues, _2) {
+            return Object.assign({}, ...keyAndValues.children.map(c => c.run()));
+        },
+        ActivityStatement(_, _1, keyAndValues, _2) {
+            return Object.assign({}, ...keyAndValues.children.map(c => c.run()));
+        },
+        PersonStatement(_, _1, keyAndValues, _2) {
+            return Object.assign({}, ...keyAndValues.children.map(c => c.run()));
+        },
+        ResourceStatement(_, _1, keyAndValues, _2) {
             return Object.assign({}, ...keyAndValues.children.map(c => c.run()));
         },
         KeyAndValue(key, _1, value, _2) {
@@ -21,14 +40,21 @@ if (m.succeeded()) {
             return this.sourceString
         },
         Value(_1) {
-            return this.sourceString
+            return `${_1.ctorName}` === 'ArrayLiteral'
+                ? _1.run()
+                : _1.sourceString
         },
+        ArrayLiteral(_1, list, _2) {
+            console.log("ArrayLiteral", list.sourceString)
+            return list.asIteration().children.map(c => c.sourceString)
+        }
     })
 
     const adapter = semantics(m);
-    const ast = adapter.run();
+    const result = adapter.run();
 
-    console.log("Result \n", ast);
+    console.log('¡Nice!😁. Project plus plus was compile successfully.');
+    console.log("Result \n", result);
 } else {
     console.log("wrong syntax ☹️! \n");
     console.log(m.message)
